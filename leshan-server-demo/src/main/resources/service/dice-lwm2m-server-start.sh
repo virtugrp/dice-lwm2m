6b59e0100ac05e7188d7185361e81a4cde15f00f
Taner <basaranntaner@gmail.com>
2020-09-01 22:38:55 +0000

    lwm2m update

--- leshan-server-demo/src/main/resources/service/dice-lwm2m-server-start.sh
+++ leshan-server-demo/src/main/resources/service/dice-lwm2m-server-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo /usr/bin/java -jar /opt/dice-lwm2m/leshan-server-demo/target/leshan-server-demo-2.0.0-SNAPSHOT-jar-with-dependencies.jar
+sudo /usr/bin/java -jar /opt/dice-lwm2m/leshan-server-demo/target/leshan-server-demo-2.0.0-SNAPSHOT-jar-with-dependencies.jar -lp 5685 -slp 5686

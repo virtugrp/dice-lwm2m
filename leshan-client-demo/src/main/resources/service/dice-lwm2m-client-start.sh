55a6b48264745b97df23cac0567af7196a769e3d
Taner <basaranntaner@gmail.com>
2020-09-01 23:03:13 +0000

    start.sh updated

--- leshan-client-demo/src/main/resources/service/dice-lwm2m-client-start.sh
+++ leshan-client-demo/src/main/resources/service/dice-lwm2m-client-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo /usr/bin/java -jar /opt/dice-lwm2m/leshan-client-demo/target/leshan-client-demo-2.0.0-SNAPSHOT-jar-with-dependencies.jar -lp 5685
+sudo /usr/bin/java -jar /opt/dice-lwm2m/leshan-client-demo/target/leshan-client-demo-2.0.0-SNAPSHOT-jar-with-dependencies.jar -u localhost:5685

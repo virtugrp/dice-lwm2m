316ef9fe372d59ff26db617c4a88485e1d8ce421
basaranntaner <basarann.taner@gmail.com>
2020-09-01 22:59:00 +0300

    Start services added for lwm2m server and client

---
+++ leshan-client-demo/src/main/resources/service/dice-lwm2m-client-start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo /usr/bin/java -jar /opt/dice-lwm2m/leshan-client-demo/target/leshan-client-demo-2.0.0-SNAPSHOT-jar-with-dependencies.jar
---
+++ leshan-server-demo/src/main/resources/service/dice-lwm2m-server-start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo /usr/bin/java -jar /opt/dice-lwm2m/leshan-server-demo/target/leshan-server-demo-2.0.0-SNAPSHOT-jar-with-dependencies.jar
